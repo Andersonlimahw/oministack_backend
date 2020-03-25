633d25bca15f64b5d73d8d9c24e5c67a4f508dc8
Anderson Lima <andersonlimahw@gmail.com>
2020-03-24 23:59:37 -0300

    [FEAT] - Cors config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
 // Define que o input é um json
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json());
 app.use(routes);
 app.listen(3333);
